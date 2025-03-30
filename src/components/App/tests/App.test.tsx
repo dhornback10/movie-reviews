23b1877d28811b7ea6a8659426cfd40258c6c097
David Hornback <dhornback10@gmail.com>
2025-03-30 15:18:23 -0400

    First test passing, jest configured

--- src/components/App/tests/App.test.tsx
+++ src/components/App/tests/App.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import App from "../App";
 
-describe("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("NYT Movie Reviews");
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  it("renders the app", () => {
+    render(<App />);
+    const title = screen.getByText("NYT Movie Reviews");
+    expect(title).toBeInTheDocument();
+  });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+// Polyfill TextEncoder and TextDecoder for the test environment
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
